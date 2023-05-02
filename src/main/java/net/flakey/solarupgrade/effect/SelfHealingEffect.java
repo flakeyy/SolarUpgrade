e3a228263724417603f8e6e8fb29c33ff3f94062
Dylan <dylanlhanna8@gmail.com>
2023-05-01 20:26:43 -0700

    update version to 0.7.0

--- src/main/java/net/flakey/solarupgrade/effect/SelfHealingEffect.java
+++ src/main/java/net/flakey/solarupgrade/effect/SelfHealingEffect.java
@@ -16,8 +16,6 @@ public class SelfHealingEffect extends MobEffect {
         if (!pLivingEntity.level.isClientSide()) {
             if (pLivingEntity.getHealth() < pLivingEntity.getMaxHealth()) {
                 pLivingEntity.heal(1.0F);
-                addAttributeModifier(Attributes.MOVEMENT_SPEED, "91AEAA56-376B-4498-935B-2F7F68070635",
-                        1.00F, AttributeModifier.Operation.MULTIPLY_TOTAL);
             }
         }
 
